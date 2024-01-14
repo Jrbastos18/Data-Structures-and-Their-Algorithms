06d871ed0610914964d44ead3a6e0c3962a54e5d
Zenalvo Junior <juniorrosalvo2015@gmail.com>
2024-01-14 15:24:30 -0300

    Included exercise number 1.3 (factorial) with recursive function

---
+++ Chapter1/Factorial/Factorial/Program.cs
@@ -0,0 +1,23 @@
+﻿namespace Factoria
+{
+    internal class Program
+    {
+        public static int Factorial(int i)
+        {
+            if (i == 1 || i == 0)
+            {
+                return 1;
+            }
+            return i * Factorial(i - 1);
+        }
+
+        static void Main(string[] args)
+        {
+            int number = int.Parse(Console.ReadLine());
+
+            int result = Factorial(number);
+
+            Console.WriteLine($"The factorial of {number} is: {result}");
+        }
+    }
+}
